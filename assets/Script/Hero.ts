9775ded5f5639bba6b9b954a67b9fe349f1a4985
szrpf <27185709@qq.com>
2023-05-24 13:06:43 +0800

    v2.0

--- assets/Script/Hero.ts
+++ assets/Script/Hero.ts
@@ -2,9 +2,23 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class Helloworld extends cc.Component {
-
     hp = 100;
     mp = 60;
+    equip = {
+        name:'装备',
+        sword:{
+            name:'小刀',
+            attack:30
+        },
+        shoe:{
+            name:'草鞋',
+            attr:{
+                name:'属性',
+                speed:5,
+                durable:100
+            }
+        }
+    }
 
     update (dt) {
         this.hp += 1.0*dt;
